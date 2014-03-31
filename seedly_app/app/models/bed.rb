33e98cc056735935ff4b049ff865e9954e256c45
cohart <me@colin-hart.com>
2014-03-31 09:55:01 -0400

    fixed models

--- seedly_app/app/models/bed.rb
+++ seedly_app/app/models/bed.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: beds
+#
+#  id          :integer          not null, primary key
+#  name        :text
+#  description :text
+#  user_id     :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Bed < ActiveRecord::Base
   has_many :seeds
   belongs_to :user
--- seedly_app/app/models/user.rb
+++ seedly_app/app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :text
+#  email           :text
+#  frostdate_id    :integer
+#  password_digest :string(255)
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
 class User < ActiveRecord::Base
   has_many :seeds, through: :beds
   has_many :beds
